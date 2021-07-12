08505c500613c56eeb0f7a57036bfed955699cba
Zane Thorn <zane.thorn@lucinahealth.com>
2021-07-12 09:50:04 -0400

    application shell created

---
+++ MapMaker/CustomCommands.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Input;
+
+namespace MapMaker
+{
+    public static class CustomCommands
+    {
+        public static RoutedCommand Exit { get; }= new RoutedCommand("Exit", typeof(CustomCommands));
+    }
+}
---
+++ MapMaker/MapEditor.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MapMaker
+{
+    public partial class MapEditor : Page
+    {
+        public MapEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
